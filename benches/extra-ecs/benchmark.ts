6fd581116e96ad80b3aa2f991921ab9e70de9dbd
BlackGlory <woshenmedoubuzhidao@blackglory.me>
2022-09-09 07:07:57 +0000

    perf: refactor benchmark

--- benches/extra-ecs/benchmark.ts
+++ benches/extra-ecs/benchmark.ts
@@ -1,32 +1,38 @@
+import { go } from '@blackglory/prelude'
 import { addRemove } from './add-remove'
 import { entityCycle } from './entity-cycle'
 import { fragIter } from './frag-iter'
 import { packed5 } from './packed-5'
 import { simpleIter } from './simple-iter'
+import { Benchmark } from 'extra-benchmark'
 
-runBench(packed5, 1000)
-runBench(simpleIter, 1000)
-runBench(fragIter, 100)
-runBench(entityCycle, 1000)
-runBench(addRemove, 1000)
+const benchmark = new Benchmark('extra-ecs', {
+  warmUps: 1000
+, runs: 5000
+})
 
-function runBench(fn: (count: number) => () => void, count: number): void {
-  const nextFrame = fn(count)
-  // pre-warm
-  for (let i = 100; i--;) {
-    nextFrame()
+benchmark.addCase('packed_5', () => {
+  return packed5(1000)
+})
+
+benchmark.addCase('simple_iter', () => {
+  return simpleIter(1000)
+})
+
+benchmark.addCase('frage_iter', () => {
+  return fragIter(100)
+})
+
+benchmark.addCase('entity_cycle', () => {
+  return entityCycle(1000)
+})
+
+benchmark.addCase('add_remove', () => {
+  return addRemove(1000)
+})
+
+go(async () => {
+  for await (const result of benchmark.run()) {
+    console.log(result)
   }
-
-  const startTime = process.hrtime.bigint()
-  const oneSecond = 1000n * 1000n * 1000n
-  let frames = 0
-  let endTime = startTime
-  while (endTime < startTime + oneSecond) {
-    nextFrame()
-    frames++
-    endTime = process.hrtime.bigint()
-  }
-  const elapsedSeconds = Number(endTime - startTime) / 1000 / 1000 / 1000
-  const framesPerSecond = Math.floor(frames / elapsedSeconds)
-  console.log(`${fn.name}: ${framesPerSecond} fps`)
-}
+})
